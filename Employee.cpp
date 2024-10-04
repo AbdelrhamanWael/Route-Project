25b6e06d6e869e32b60de6a99ad3248588f35071
AbdelrhamanWael <108825425+AbdelrhamanWael@users.noreply.github.com>
2024-10-04 19:42:46 +0300

    22

--- Employee.cpp
+++ Employee.cpp
@@ -1,40 +1,17 @@
+
 #include <iostream>
 #include <fstream>
-#include "Client.h"
-#include "Employee.h"
-#include "Admin.h"
 #include <string>
+#include "Employee.h"
+#include"Admin.h"
+#include "Client.h"
+#include "Data.h"
+
 using namespace std;
 
-int main()
-{
-    //========================== For Client Class====================================
-    Client c;
-    string name;
-    string password;
-    int balance;
-    int id;
 
-    cout << "Enter Client Name: ";
-    cin >> name;
-    c.setName(name);
+int main(){
 
-    cout << "Enter Client ID: ";
-    cin >> id;
-    c.setId(id);
-
-    cout << "Enter Client Password: ";
-    cin >> password;
-    c.setPassword(password);
-
-    cout << "Enter Client Balance: ";
-    cin >> balance;
-    c.setBalance(balance);
-    cout << "===========================================" << endl;
-
-    // c.Displayclientinfo();
-
-    //============================== For Employee Class=================================
     string name;
     string password;
     int balance;
@@ -148,6 +125,5 @@ else
     }
     
 
-   
 
 }
