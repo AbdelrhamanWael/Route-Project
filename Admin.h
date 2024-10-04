38e6ba0d4cffdcad2ea11cde166d835939e57844
AbdelrhamanWael <108825425+AbdelrhamanWael@users.noreply.github.com>
2024-10-04 16:05:41 +0300

    phase()

--- Admin.h
+++ Admin.h
@@ -1,10 +1,12 @@
+
 #pragma once
 #ifndef ADMIN_H
 #define ADMIN_H
 #include <iostream>
-#include "Person.h"
-#include "Validation.h"
 #include <string>
+#include "Client.h"
+#include "Validation.h"
+
 
 using namespace std;
 
@@ -12,6 +14,8 @@ class Admin : public Person
 {
 private:
     double salary;
+    vector<Client*> clients;
+    vector<Employee*> employees;
 
 public:
     Admin() { this->salary = 0; }
@@ -42,6 +46,11 @@ public:
 
         this->salary = salary;
     }
+    
+    void setbalance(double balance)
+    {
+        this->salary = balance;
+    }
     void setId(int id)
     {
         this->id = id;
@@ -58,6 +67,111 @@ public:
 
         this->password = password;
     }
+
+    void addClient(Client* client)
+    {
+        clients.push_back(client);
+
+    }
+    
+    //search
+    bool searchClient(int id)
+    {
+        for (Client* client : clients)
+        {
+            if (client->getId() == id)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    //listofclients
+    void listofclients()
+    {
+        for (Client* client : clients)
+        {
+            client->Displayclientinfo();
+        }
+    }
+
+
+    void editClient(int id, string name, string password, double balance){
+        for (int i = 0; i < clients.size(); i++)
+        {
+            if (clients[i]->getId() == id)
+            {
+                clients[i]->setName(name);
+                clients[i]->setPassword(password);
+                clients[i]->setBalance(balance);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+    //addd Employee
+    void addEmployee(Employee* employee)
+    {
+        employees.push_back(employee);
+    }
+    //search
+    bool searchEmployee(int id)
+    {
+        for (Employee* employee : employees)
+        {
+            if (employee->getId() == id)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    //edit
+    void editEmployee(int id, string name, string password, double salary)
+    {
+        for (int i = 0; i < employees.size(); i++)
+        {
+            if (employees[i]->getId() == id)
+            {
+                employees[i]->setName(name);
+                employees[i]->setPassword(password);
+                employees[i]->setSalary(salary);
+            }
+        }
+    }
+
+    
+    void listofemployees()
+    {
+        for (Employee* employee : employees)
+        {
+            employee->DisplayEmployeeinfo();
+        }
+    }
+
+
+    
+
+
+    
+    
+
+
+
+
+
+
+
+
 
     // display
     void DisplayAdmininfo()
--- main.cpp
+++ main.cpp
@@ -35,7 +35,19 @@ int main()
     // c.Displayclientinfo();
 
     //============================== For Employee Class=================================
-    Employee e;
+    string name;
+    string password;
+    int balance;
+    int id;
+    int choice;
+    string clientName;
+    int clientId;
+    double clientBalance;
+    string Clientpassword;
+    bool clientAdded = false;
+    int idtosearch;
+
+Employee e;
 
     double salary;
 
@@ -47,20 +59,7 @@ int main()
     cin >> id;
     e.setId(id);
 
-    // for searching
-    int clientid;
-    cout << "Enter Client ID: ";
-    cin >> clientid;
-    bool found = e.searchClient(clientid);
-
-    if (found)
-    {
-        cout << "Client Already Exist" << endl;
-    }
-    else
-    {
-        cout << "Client Not Exist" << endl;
-    }
+    
 
     cout << "Enter Employee Password: ";
     cin >> password;
@@ -71,7 +70,57 @@ int main()
     e.setSalary(salary);
     cout << "===========================================" << endl;
 
-    // e.DisplayEmployeeinfo();
+
+
+cout << " Do you want to add a client? (1. Yes, 2. No)";
+cin >> choice;
+if (choice == 1)
+{
+    cout << "Enter Client Name: ";
+    cin >> clientName;
+
+    cout << "Enter Client ID: ";
+    cin >> clientId;
+
+    cout << "Enter Client Balance: ";
+    cin >> clientBalance;
+    cout << "Enter Client Password";
+    cin >> Clientpassword;
+    Client* client = new Client(clientName, clientId, clientBalance, Clientpassword);
+    e.addClient(client);
+    clientAdded = true;
+}
+else
+{
+    cout << "No Client Added" << endl;
+}
+
+if (clientAdded)
+{
+    cout << "======================================" << endl;
+
+    cout << "do you want to search for client ?  (1. Yes, 2. No)";
+    cin >> choice;
+    if (choice == 1)
+    {
+        cout << "Enter client id to search for: ";
+        cin >> idtosearch;
+        e.searchClient(idtosearch);
+    }
+    else
+    {
+        cout << "Client Search Cancelled" << endl;
+    }
+}
+else
+{
+    cout << "Exiting Program..." << endl;
+    return 0; // or exit(0);
+}
+
+
+
+
 
     ofstream outputFile1("employee_data.txt");
 
@@ -82,16 +131,39 @@ int main()
         outputFile1 << "Employee ID: " << id << endl;
         outputFile1 << "Employee Password: " << password << endl;
         outputFile1 << "Employee Salary: " << salary << endl;
-
+        outputFile1 << "=================================================" << endl;
+        // Write client data to the file
+        outputFile1 << "Client that You added ::" << endl;
+        outputFile1 << "Client Name: " << clientName << endl;
+        outputFile1 << "Client ID: " << clientId << endl;
+        outputFile1 << "Client Balance: " << clientBalance << endl;
+        outputFile1 << "Client Password: " << Clientpassword << endl;
         outputFile1.close();
         cout << "data saved Successfully" << endl;
     }
     else
     {
         cout << "Unable to open file";
+
     }
+    
 
     //===============================Admin Class====================================
+string name;
+    string password;
+    double balance;
+    double salary;
+    int id;
+    int choice;
+    string clientName;
+    int clientId;
+    double clientBalance;
+    string Clientpassword;
+    int idtosearch;
+    bool employeeAdded = false;
+    bool clientAdded = false;
+    
+    
 
     Admin a;
 
@@ -109,16 +181,139 @@ int main()
 
     cout << "Enter Admin Balance: ";
     cin >> balance;
-    a.setSalary(balance);
+    a.setbalance(balance);
 
-    c.setBalance(balance);
+    
 
-    c.Displayclientinfo();
 
-    cout << endl;
-    e.DisplayEmployeeinfo();
-    cout << endl;
-    a.DisplayAdmininfo();
+    cout << "===========================================" << endl;
 
-    return 0;
+
+cout << " Do you want to add a client? (1. Yes, 2. No)";
+cin >> choice;
+if (choice == 1)
+{
+    cout << "Enter Client Name: ";
+    cin >> clientName;
+
+    cout << "Enter Client ID: ";
+    cin >> clientId;
+
+    cout << "Enter Client Balance: ";
+    cin >> clientBalance;
+    cout << "Enter Client Password";
+    cin >> Clientpassword;
+    Client* client = new Client(clientName, clientId, clientBalance, Clientpassword);
+    a.addClient(client);
+    clientAdded = true;
+}
+else
+{
+    cout << "No Client Added" << endl;
+}
+
+if (clientAdded)
+{
+    cout << "======================================" << endl;
+
+    cout << "do you want to search for client ?  (1. Yes, 2. No)";
+    cin >> choice;
+    if (choice == 1)
+    {
+        cout << "Enter client id to search for: ";
+        cin >> idtosearch;
+        a.searchClient(idtosearch);
+    }
+    else
+    {
+        cout << "Client Search Cancelled" << endl;
+    }
+}
+else
+{
+    cout << "Exiting Program..." << endl;
+    return 0; // or exit(0);
+}
+    
+        
+    
+
+    cout << "=====================================" << endl;
+    //add employee
+
+    
+
+
+cout << "do you want to add employee ?  (1. Yes, 2. No)";
+cin >> choice;
+if (choice == 1)
+{
+    cout << "Enter employee name: ";
+    cin >> name;
+    cout << "Enter employee id: ";
+    cin >> id;
+    cout << "Enter employee password: ";
+    cin >> password;
+    cout << "Enter employee salary: ";
+    cin >> salary;
+    Employee* employee = new Employee(name, password, id, salary, balance);
+    a.addEmployee(employee);
+    employeeAdded = true;
+}
+else
+{
+    cout << "Employee Not Added" << endl;
+}
+
+if (employeeAdded)
+{
+    cout << "======================================" << endl;
+
+    cout << "do you want to search for employee ?  (1. Yes, 2. No)";
+    cin >> choice;
+    if (choice == 1)
+    {
+        cout << "Enter employee id to search for: ";
+        cin >> idtosearch;
+        a.searchEmployee(idtosearch);
+    }
+    else
+    {
+        cout << "Employee Search Cancelled" << endl;
+    }
+}
+else
+{
+    cout << "Exiting Program..." << endl;
+    return 0; 
+}
+ofstream outputFile1("Admin_data.txt");
+
+if (outputFile1.is_open())
+{
+    // Write admin data to the file
+    outputFile1 << "Admin Name: " << name << endl;
+    outputFile1 << "Admin ID: " << id << endl;
+    outputFile1 << "Admin Salary: " << salary << endl;
+    outputFile1 << "======================================" << endl;
+    // Write client data to the file
+    outputFile1 << "Client that You added ::" << endl;
+    outputFile1 << "Client Name: " << clientName << endl;
+    outputFile1 << "Client ID: " << clientId << endl;
+    outputFile1 << "Client Balance: " << clientBalance << endl;
+    outputFile1 << "Client Password: " << Clientpassword << endl;
+    outputFile1 << "======================================" << endl;
+    // Write employee data to the file
+    outputFile1 << "Employee that You added ::" << endl;
+    outputFile1 << "Employee Name: " << name << endl;
+    outputFile1 << "Employee ID: " << id << endl;
+    outputFile1 << "Employee Salary: " << salary << endl;
+    outputFile1 << "======================================" << endl;
+    outputFile1.close();
+    cout << "data saved Successfully" << endl;
+}
+else
+{
+    cout << "Unable to open file";
+    
 }
